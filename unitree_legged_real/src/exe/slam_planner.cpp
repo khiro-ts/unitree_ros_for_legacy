0c35320027a1b64c176d6b44600c2a639d152da6
Wataru Oshima <watruoshima@my.smccd.edu>
2021-07-28 12:11:57 +0900

    comment the move::base

--- unitree_legged_real/src/exe/slam_planner.cpp
+++ unitree_legged_real/src/exe/slam_planner.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv){
   
   tf2_ros::Buffer buffer(ros::Duration(10));
   tf2_ros::TransformListener tf(buffer);
-  move_base::MoveBase planner( buffer );
+  //move_base::MoveBase planner(buffer);
 
   ros::spin();
   return(0);
